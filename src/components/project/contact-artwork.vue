4b9e5714eb3ac40299a8d817fa089e017e5c58c1
metamn <bartus.csongor@gmail.com>
2017-10-13 11:28:17 +0300

    Working on contact artwork

--- src/components/project/contact-artwork.vue
+++ src/components/project/contact-artwork.vue
@@ -1,17 +1,51 @@
 <template lang="html">
-  <mv-text-around-circle text="• Follow • Contact" width="12.5em" height="12.5em"/>
+  <article :class="$style.contactArtwork">
+    <h3 hidden>Contact artwork</h3>
+
+    <mv-text-around-circle :class="$style.textAroundCircle" text="• Follow • Contact" width="12.5em" height="12.5em"/>
+    <mv-image :class="$style.image" :image="image"/>
+  </article>
 </template>
 
 <script>
   import textAroundCircle from '../framework/text-around-circle'
+  import image from '../framework/image'
 
   export default {
     name: 'mv-contact-artwork',
     components: {
-      'mv-text-around-circle': textAroundCircle
+      'mv-text-around-circle': textAroundCircle,
+      'mv-image': image
+    },
+    data: function () {
+      return {
+        image: {
+          src: '/static/images/email.png',
+          title: 'Email'
+        }
+      }
     }
   }
 </script>
 
-<style lang="css">
+<style module>
+  .contactArtwork {
+    position: relative;
+    width: 12.5em;
+    height: 12.5em;
+  }
+
+  .image,
+  .textAroundCircle {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  .image {
+    width: calc(12.5em / 1.5) !important;
+    height: calc(12.5em / 1.5) !important;
+    top: calc((12.5em - calc(12.5em / 1.5)) / 2);
+    left: calc((12.5em - calc(12.5em / 1.5)) / 2);
+  }
 </style>
