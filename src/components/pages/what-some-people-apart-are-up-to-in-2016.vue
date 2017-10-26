c7ff8077dadfcd25b17ad55f81ef106aa47ebcff
metamn <bartus.csongor@gmail.com>
2017-10-26 16:17:41 +0300

    Working on a new article ALA2016

--- src/components/pages/what-some-people-apart-are-up-to-in-2016.vue
+++ src/components/pages/what-some-people-apart-are-up-to-in-2016.vue
@@ -227,6 +227,206 @@
                 avatar: '/static/images/lara_hogan.jpg'
               },
               quote: 'Fears about public speaking.'
+            },
+            {
+              person: {
+                name: 'Denise Jacobs',
+                title: 'Founder and CEO of The Creative Dose; speaker + author + creativity evangelist',
+                url: 'http://denisejacobs.com/',
+                twitter: 'denisejacobs',
+                avatar: '/static/images/denisejacobs.jpeg'
+              },
+              quote: 'Free people from the tyranny of their inner critic.'
+            },
+            {
+              person: {
+                name: 'Erin Lynch',
+                title: 'Writer / designer / founder of shop, and production manager at A List Apart',
+                url: 'http://www.webclique.net',
+                twitter: 'erinlynch',
+                avatar: '/static/images/erinlynch.jpg'
+              },
+              quote: 'Growth'
+            },
+            {
+              person: {
+                name: 'Sophie Shepherd',
+                title: 'Designer at GitHub',
+                url: 'http://sophieshepherd.com',
+                twitter: 'sophshepherd',
+                avatar: '/static/images/sophshepherd.jpg'
+              },
+              quote: 'Push the boundaries of the medium (web)'
+            },
+            {
+              person: {
+                name: 'Rachel Andrew',
+                title: 'Founder of edgeofmyseat.com, the company behind Perch',
+                url: 'http://rachelandrew.co.uk/',
+                twitter: 'rachelandrew',
+                avatar: '/static/images/rachelandrew.jpg'
+              },
+              quote: 'Javascript'
+            },
+            {
+              person: {
+                name: 'Anthony Colangelo',
+                title: 'iOS developer, Big Cartel',
+                url: 'https://acolangelo.com/',
+                twitter: 'acolangelo',
+                avatar: '/static/images/acolangelo.jpg'
+              },
+              quote: 'Hardware and 3-D printing'
+            },
+            {
+              person: {
+                name: 'Garin Evans',
+                title: 'Developer',
+                url: 'http://www.garinevans.com',
+                twitter: 'mrgarinevans',
+                avatar: '/static/images/mrgarinevans.jpeg'
+              },
+              quote: 'React Native'
+            },
+            {
+              person: {
+                name: 'Lyza Danger Gardner',
+                title: 'CTO, Cloud Four',
+                url: 'https://www.lyza.com',
+                twitter: 'lyzadanger',
+                avatar: '/static/images/lyzadanger.jpeg'
+              },
+              quote: 'Physical Web, HTML controlled hardware'
+            },
+            {
+              person: {
+                name: 'Matt Griffin',
+                title: 'Founder of Bearded',
+                url: 'http://www.bearded.com/',
+                twitter: 'elefontpress',
+                avatar: '/static/images/elefontpress.png'
+              },
+              quote: 'Accessibility'
+            },
+            {
+              person: {
+                name: 'Krystal Higgins',
+                title: 'Senior interaction designer',
+                url: 'http://www.kryshiggins.com/',
+                twitter: 'kryshiggins',
+                avatar: '/static/images/kryshiggins.jpg'
+              },
+              quote: 'Physical Web, HTML controlled hardware'
+            },
+            {
+              person: {
+                name: 'Ryan Irelan',
+                title: 'Builder of Mijingo',
+                url: 'http://ryanirelan.com',
+                twitter: 'ryanirelan',
+                avatar: '/static/images/ryanirelan.jpg'
+              },
+              quote: 'Decoupled content management systems'
+            },
+            {
+              person: {
+                name: 'Scott Jehl',
+                title: 'Designer / developer at Filament Group and author of Responsible Responsive Design',
+                url: 'http://scottjehl.com',
+                twitter: 'scottjehl',
+                avatar: '/static/images/scottjehl.jpg'
+              },
+              quote: 'HTTP2 and Service Workers'
+            },
+            {
+              person: {
+                name: 'Peter-Paul Koch',
+                title: 'Mobile platform strategist',
+                url: 'http://www.quirksmode.org',
+                twitter: 'ppk',
+                avatar: '/static/images/ppk.jpeg'
+              },
+              quote: 'Installable web apps'
+            },
+            {
+              person: {
+                name: 'Una Kravets',
+                title: 'Front-end developer at IBM Design, Austin',
+                url: 'http://twitter.com/una',
+                twitter: 'Una',
+                avatar: '/static/images/una.jpg'
+              },
+              quote: 'Javascript, Web images, Offline web apps'
+            },
+            {
+              person: {
+                name: 'Jeff Lembeck',
+                title: 'www engineer at npm, Inc.',
+                url: 'https://gist.github.com/jefflembeck/2eac4a73baec1af658b7',
+                twitter: 'jefflembeck',
+                avatar: '/static/images/jefflembeck.jpg'
+              },
+              quote: 'Node JS'
+            },
+            {
+              person: {
+                name: 'Mark Llobrera',
+                title: 'Technology director, Bluecadet',
+                url: 'http://alistapart.com/author/mllobrera',
+                twitter: 'dirtystylus',
+                avatar: '/static/images/dirtystylus.jpeg'
+              },
+              quote: 'Learning'
+            },
+            {
+              person: {
+                name: 'Paul Robert Lloyd',
+                title: 'Independent graphic designer and web developer',
+                url: 'https://paulrobertlloyd.com',
+                twitter: 'paulrobertlloyd',
+                avatar: '/static/images/paulrobertlloyd.png'
+              },
+              quote: 'Javascript'
+            },
+            {
+              person: {
+                name: 'Sarah Parmenter',
+                title: 'Designer and founder, You Know Who',
+                url: 'http://twitter.com/sazzy',
+                twitter: 'sazzy',
+                avatar: '/static/images/sazzy.jpg'
+              },
+              quote: 'Programming && Honest design and branding'
+            },
+            {
+              person: {
+                name: 'Simon St. Laurent',
+                title: 'Strategic description director at O’Reilly Media, Inc.',
+                url: 'http://www.simonstl.com',
+                twitter: '',
+                avatar: ''
+              },
+              quote: 'The borderlands between best practices for app development and site development.'
+            },
+            {
+              person: {
+                name: 'Ian Vanhoof',
+                title: 'Technical editor at A List Apart',
+                url: 'http://twitter.com/ianvanhoof',
+                twitter: 'ianvanhoof',
+                avatar: '/static/images/ianvanhoof.jpg'
+              },
+              quote: 'HTTP2'
+            },
+            {
+              person: {
+                name: 'Aarron Walter',
+                title: 'Vice president of R&D at MailChimp',
+                url: 'http://twitter.com/aarron',
+                twitter: 'aarron',
+                avatar: '/static/images/aarron.jpg'
+              },
+              quote: 'Sharpening my tools'
             }
           ]
         }
